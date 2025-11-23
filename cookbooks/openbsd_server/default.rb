e3285ad1ced77a521b36339d1b95798f8c107bb8
Alexander D. <maniac@pzskc383.net>
2025-11-23 13:34:11 +0200

    fixes for openbsd_package plugin

--- cookbooks/openbsd_server/default.rb
+++ cookbooks/openbsd_server/default.rb
@@ -3,6 +3,7 @@
 openbsd_package "vim" do
   action :install
   flavor "no_x11"
+  # version "9.1.1706"
 end
 
 openbsd_package "nnn" do
--- plugins/mitamae-plugin-resource-openbsd_package/mrblib/resource_executor/openbsd_package.rb
+++ plugins/mitamae-plugin-resource-openbsd_package/mrblib/resource_executor/openbsd_package.rb
@@ -3,7 +3,8 @@ puts "puts from openbsd_package/resource_executor.rb"
 module ::MItamae
   module Plugin
     module ResourceExecutor
-      OpenBSDPackageError = Class.new(RuntimeError)
+      OpenBSDPackageError = Class.new(::RuntimeError)
+
       class OpenBSDPackage < ::MItamae::ResourceExecutor::Base
         VERSION_RE = %r{\A
           (?<name>
@@ -11,12 +12,12 @@ module ::MItamae
           )-
           (?<version>
             [0-9.]+
-            (?:(?:rc|alpha|beta|pre|pl|p)[0-9]+)?
+            ((rc|alpha|beta|pre|pl|p)[0-9]+)?
           )
           (?<flavor>
-            (?<=-)
             [a-z0-9_-]+
           )?
+          ,.*
         \Z}x
 
         FUZZY_RE = %r{\A
@@ -33,6 +34,9 @@ module ::MItamae
           \Z}x
 
         def apply
+          # ::MItamae.logger.debug "Desired: #{desired.inspect}"
+          # ::MItamae.logger.debug "Current: #{current.inspect}"
+
           if desired.installed && !current.installed
             install_package(desired)
           elsif !desired.installed && current.installed
@@ -46,8 +50,17 @@ module ::MItamae
         private
 
         def attributes_changed?
-          %i[version branch flavor].each do |m|
-            return true if current.send(m) != desired.send(m)
+          attrs_to_check = %i[branch flavor]
+          # we check version only if version specified
+          if desired.version
+            attrs_to_check.push(:version)
+          end
+
+          attrs_to_check.each do |m|
+            if current.send(m) != desired.send(m)
+              ::MItamae.logger.debug "#{m} changed! #{current.send(m)} != #{desired.send(m)}"
+              return true
+            end
           end
           false
         end
@@ -55,9 +68,8 @@ module ::MItamae
         def set_current_attributes(current, _action)
           installed_info = installed_info(attributes.name)
 
-          current.installed = !installed_info.nil?
-          %i[version branch flavor].each do |k|
-            current.send("#{k}=", installed_info[k])
+          %i[name installed version branch flavor].each do |k|
+            current.send("#{k}=", installed_info[k]) unless installed_info[k].nil?
           end
         end
 
@@ -71,8 +83,11 @@ module ::MItamae
         end
 
         def installed_info(pkg_name)
-          info = {name: pkg_name}
-          fuzzy_check_result = run_command(["pkg_info", "-qze", "#{pkg_name}-*"])
+          info = {name: pkg_name, installed: false}
+          fuzzy_check_result = run_command(["pkg_info", "-qze", "#{pkg_name}-*"], error: false)
+
+          return info if fuzzy_check_result.exit_status > 0
+          info[:installed] = true
 
           fm = FUZZY_RE.match(fuzzy_check_result.stdout.lines.first.chomp)
 
@@ -81,18 +96,20 @@ module ::MItamae
           info[:version] = get_version(pkg_name)
 
           info
-        rescue
+        rescue => e
+          ::MItamae.logger.error "Error checking installed info: #{e.inspect}"
           info
         end
 
         def get_version(pkg_name)
           version_result = run_command(["pkg_info", "-qSe", "#{pkg_name}-*"])
 
-          raise OpenBSDPackageError("Invalid version check result!") if version_result.exit_status != 0
+          raise OpenBSDPackageError.new("Invalid version check result!") if version_result.exit_status != 0
 
-          vm = VERSION_RE.match(version_result.stdout.lines.first.chomp)
+          version_line = version_result.stdout.lines.first.chomp
+          vm = VERSION_RE.match(version_line)
 
-          raise OpenBSDPackageError("Can't find version in version check output!") if !vm || !vm.has?(:version)
+          raise OpenBSDPackageError.new("Can't find version in version check output!") if !vm || vm[:version].nil?
 
           vm[:version]
         end
@@ -102,7 +119,7 @@ module ::MItamae
         end
 
         def install_package(info)
-          raise OpenBSDPackageError("Specify either branch or version") if info.version && info.branch
+          raise OpenBSDPackageError.new("Specify either branch or version") if info.version && info.branch
 
           version = info.version or ""
           pkg_name = "#{info.name}-#{version}-"
